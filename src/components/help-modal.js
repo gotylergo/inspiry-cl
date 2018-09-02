2f97f3a08fdfe1547d384e83f749f224cd4c88f3
gotylergo <tk03759@gmail.com>
2018-09-02 15:10:40 -0700

    Change Comforta font fallback to Verdana

--- src/components/help-modal.js
+++ src/components/help-modal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './modal.css';
+import './help-modal.css';
 
 class HelpModal extends Component {
     render() {
@@ -13,13 +14,12 @@ class HelpModal extends Component {
                 </div>
                 <h3>Unblock Your Brain</h3>
                 <p>The best way to beat writer's block is to get out of your head and just start writing. Inspiry turns writing into a game by giving you a topic and forces you to get creative by throwing challenge words at you. Race the clock as you create a story, and inspire yourself while having some fun.</p>
-                <ol>
-                    <li><a href="#new-story">Start a Story</a></li>
+                <ol class="toc">
+                    <li><a href="#new-story">Start Your Story</a></li>
                     <li><a href="#share-your-work">Share Your Work</a></li>
                     <li><a href="#read-for-inspiration">Read for Inspiration</a></li>
                 </ol>
-                <a name="new-story"></a>
-                <h4>Start a story</h4>
+                <a name="new-story"><h4>Start Your Story</h4></a>
                 <ol>
                     <li>Click the + icon in the toolbar.</li>
                     <li>Inspiry generates a genre for your story.</li>
@@ -29,13 +29,12 @@ class HelpModal extends Component {
                     <li>Challenge: At some point in your story, Inspiry may insert a suprise image challenge. This image is part of your story, so be sure to somehow make it relevant as you write.</li>
                     <li>When the timer runs out, your story is complete. Save it to your journal or share it with the world.</li>
                 </ol>
-                <a name="share-your-work"></a>
-                <h4>Share Your Work</h4>
+
+                <a name="share-your-work"><h4>Share Your Work</h4></a>
                 <ol>
                     <li>...</li>
                 </ol>
-                <a name="read-for-inspiration"></a>
-                <h4>Start a story</h4>
+                <a name="read-for-inspiration"><h4>Read for Inspiration</h4></a>
                 <ol>
                     <li>...</li>
                 </ol>
