7a86bd4c4eadebca993435aad566682d2abbbc14
gotylergo <tk03759@gmail.com>
2018-09-01 22:59:20 -0700

    Apply flexbox to modal to fix blurry text transform on webkit browsers

--- src/components/auth-modal.js
+++ src/components/auth-modal.js
@@ -6,12 +6,14 @@ import './modal.css';
 class AuthModel extends Component {
     render() {
         return (
-            <div className="auth-modal modal">
-                <div className="auth-menu">
-                    <span className="sign-in-button button">SIGN IN</span> | <span className="register-button button">REGISTER</span>
+            <div className="flex-centered">
+                <div className="auth-modal modal">
+                    <div className="auth-menu">
+                        <span className="sign-in-button button">SIGN IN</span> | <span className="register-button inactive-button button">REGISTER</span>
+                    </div>
+                    <Login />
+                    <Register />
                 </div>
-                <Login />
-                <Register />
             </div>
         )
     }
