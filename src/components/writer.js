fb8f2918ee61f4f1bb3a68e2a40fd154eae185b6
gotylergo <tk03759@gmail.com>
2018-09-06 00:56:42 -0700

    Cleanup writer page and fix faStopwatch

--- src/components/writer.js
+++ src/components/writer.js
@@ -9,18 +9,16 @@ class Writer extends Component {
             <div className="writer">
                 <TopBar />
                 <main className="writer-main">
-                    {/* <div className="bg-diluter"> */}
                     <div className="story-header">
-                        <div className="timer">
-                            <FontAwesomeIcon icon="Stopwatch" />
-                        </div>
+                        <span className="timer">
+                            60 <FontAwesomeIcon icon="stopwatch" />
+                        </span>
                         <h1>my <span>'{'horror'}'</span> story</h1>
                         <div className="target-word">salamander</div>
                         <div className="target-word-label">word of the sentence:</div>
                         <textarea className="story-input" placeholder="Start writing here"></textarea>
                     </div>
                     <p className="story">Curabitur facilisis leo at venenatis fringilla. In ullamcorper sagittis dui, mattis imperdiet metus commodoeget. Nulla nec erat nec placerat vestibulum. Curabitur sed dapibus Pellentesque vestibulummattis semper. Duis ultricies p metus in vestibulum. <img src="" alt="Random" /> Etiam iaculis, lacus in eleifend, antepurus sagittis magna, vitae tincidunt felis orci a eros. Integer sollicitudin ipsum et malesuada fringilla. Sed consequat, mi at euismod lacinia, magna metus pul tortor, quis semper nisi turpis quis lacus. Aeneanorci ipsum, maximus sit amet ornare eget, fermentum eu  Phasellus dui nisi, ornare a fringilla ac, ornarefringilla magna.</p>
-                    {/* </div> */}
                 </main>
                 <div className="writer-footer">
                     <button>Save</button>
